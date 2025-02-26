e092d55a1366491367020f8a6fd71734d2e197db
AlexandraHockett <alexandramandio@gmail.com>
2025-02-26 22:46:47 +0000

    add middleware

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const auth = basicAuth.split(" ")[1];
+    const [user, password] = Buffer.from(auth, "base64").toString().split(":");
+
+    if (user === "badcompany_anyfa" && password === "B@dc0mp@ny@nyf@") {
+      return NextResponse.next();
+    }
+  }
+
+  return new Response("Autenticação necessária", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Protected Area"',
+    },
+  });
+}
